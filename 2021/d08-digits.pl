63de4eb306617eaacf702ddea5f3d784f92788a6
Michael Hamlin <michaelh@ziprecruiter.com>
2021-12-08 11:55:10 -0500

    bit of cleanup

--- 2021/d08-digits.pl
+++ 2021/d08-digits.pl
@@ -17,7 +17,6 @@ use List::Util 'sum0';
 my %opt;
 GetOptions(\%opt, 
   'test',
-  'part2',   # select part 2
 ) or die "couldn't parse options: @ARGV";
 
 my %cnt;
@@ -28,9 +27,11 @@ $cnt{_lines} = my @lines = grep /^\w/, $opt{test} ?
   do {local $/; split /\n/, <DATA>} 
   : path($file)->lines({chomp => 1});
 
-my @display;  # hashrefs containing wiring data about each 4-digit display
+my @display;  # hashrefs containing wiring data about each 4-digit display:
+# patterns is arrayref of the segment patterns used for the 10 numerals
+# digits is arrayref of the segment patterns for the display we are trying to read
 
-# order segments alphabetically
+# order segment pattern alphabetically
 sub canon_seg { join '', sort split //, shift }
 
 while (my @items = split ' ', shift @lines) {
@@ -55,7 +56,11 @@ sub find_length ($len, @list) {
 # number of segments lit for each numeral 0-9
 #            0  1  2  3  4  5  6  7  8  9
 my @nsegs = (6, 2, 5, 5, 4, 5, 6, 3, 7, 6);
-# in-place re-ordering of arrayref of patterns:
+# this function does an in-place re-ordering of arrayref of patterns:
+# after this, patterns for numerals 1 4 7 8 are in corresponding positions 
+# in the array, and all other positions have patterns that match the 
+# required lengths.  we'll swap those around later once we deduce which 
+# is which.
 sub re_sort ($patterns) {
   foreach my $numeral (reverse 0 .. 9) { # (8, 7, 4, 1)
     my $saved = $patterns->[$numeral];
@@ -66,7 +71,7 @@ sub re_sort ($patterns) {
     $patterns->[$pos] = $saved;
   }
 }
-# solve for 1, 4, 7, 8
+# replace some patterns in the digits with actual numerals 1 4 7 8
 sub solve_easy_digits ($digits) {
   my %digitmap = (2 => 1, 4 => 4, 3 => 7, 7 => 8);
   @$digits = map {$digitmap{length $_} // $_} @$digits ;
@@ -74,6 +79,7 @@ sub solve_easy_digits ($digits) {
 
 sub solve_segments ($patterns) {
   my @segmap;  # we could save this, but don't need it externally...
+  # segmap contains the letter that represents each segment (a .. g)
   $segmap[0] = eval "'$patterns->[7]' =~ tr/$patterns->[1]//dr";  # a
   foreach my $six (0, 6, 9) {
     my $nonseven = eval "'$patterns->[$six]' =~ tr/$patterns->[7]//dr";
@@ -85,47 +91,46 @@ sub solve_segments ($patterns) {
     last;
   }
   $segmap[5] = $patterns->[1] =~ s/$segmap[2]//r; # f
-  foreach my $six (0, 9) {
-    my $nonfour = eval "'$patterns->[$six]' =~ tr/$patterns->[4]//dr";
+  foreach my $nine (0, 9) {
+    my $nonfour = eval "'$patterns->[$nine]' =~ tr/$patterns->[4]//dr";
     next unless length $nonfour == 2;
-    $segmap[4] = eval "'abcdefg' =~ tr/$patterns->[$six]//dr";  # e
-    unless ($six == 9) {  # identified the numeral 9
-      ($patterns->[9], $patterns->[$six]) = ($patterns->[$six], $patterns->[9]);
+    $segmap[4] = eval "'abcdefg' =~ tr/$patterns->[$nine]//dr";  # e
+    unless ($nine == 9) {  # identified the numeral 9
+      ($patterns->[9], $patterns->[$nine]) = ($patterns->[$nine], $patterns->[9]);
     }
     last;
   }
   $segmap[3] = eval "'abcdefg' =~ tr/$patterns->[0]//dr"; # d
-  foreach my $five (2, 3, 5) {
-    my $nonnine = eval "'$patterns->[$five]' =~ tr/$patterns->[9]//dr";
+  foreach my $two (2, 3, 5) {
+    my $nonnine = eval "'$patterns->[$two]' =~ tr/$patterns->[9]//dr";
     next unless length $nonnine;  # nine has _all_ segments of the 3 and 5
-    unless ($five == 2) {  # identified the numeral 2
-      ($patterns->[2], $patterns->[$five]) = ($patterns->[$five], $patterns->[2]);
+    # we already knew which letter was segment e
+    unless ($two == 2) {  # identified the numeral 2
+      ($patterns->[2], $patterns->[$two]) = ($patterns->[$two], $patterns->[2]);
     }
     last;
   }
   # two segments left: b, g
   my $found = join '', sort grep defined, @segmap;
-  foreach my $five (3, 5) {
-    my $nonfound = eval "'$patterns->[$five]' =~ tr/$found//dr";
+  foreach my $three (3, 5) {
+    my $nonfound = eval "'$patterns->[$three]' =~ tr/$found//dr";
     next unless length $nonfound == 1;
     $segmap[6] = $nonfound;  # segment g
-    unless ($five == 3) {  # identified the numeral 3
-      ($patterns->[3], $patterns->[$five]) = ($patterns->[$five], $patterns->[3]);
+    unless ($three == 3) {  # identified the numeral 3
+      ($patterns->[3], $patterns->[$three]) = ($patterns->[$three], $patterns->[3]);
     }
     last;
   }
 
-  # only one segment left
+  # only one segment left: b
   $found = join '', sort grep defined, @segmap;
-  $segmap[1] = eval "'abcdefg' =~ tr/$found//dr";  # g
+  $segmap[1] = eval "'abcdefg' =~ tr/$found//dr";  # b
   #print STDERR 'segments: '; p @segmap;
-  #print STDERR 'solved patterns: ';
-  #p $patterns;
+  #print STDERR 'solved patterns: '; p $patterns;
 }
-sub solve_digits ($display) {
-  # by now, all patterns are in proper place
+sub set_reading ($display) {
+  # by now, all patterns are in proper place, so we can just translate
   my %pmap = reverse map { $_ => $display->{patterns}[$_] } 0 .. 9;
-  # p %pmap;
   $display->{reading} = join '', map {$pmap{$_} // $_} @{$display->{digits}};
 }
 
@@ -134,7 +139,7 @@ foreach my $disp (@display) {
   re_sort($disp->{patterns});
   solve_easy_digits($disp->{digits});
   solve_segments($disp->{patterns});
-  solve_digits($disp);
+  set_reading($disp);
   say "reading $disp->{reading} came from patterns: @{$disp->{digits}}";
 }
 
